d8240a269020f1b412a41c665afec94b2e27eb82
Manuel Artero Anguita <manutero.developer@gmail.com>
2024-09-18 17:03:14 +0200

    try-catch for intial state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState(() => {
-    const savedTodos = localStorage.getItem("todos");
-    return savedTodos ? JSON.parse(savedTodos) : [];
+    try {
+      const savedTodos = localStorage.getItem("todos");
+      return savedTodos ? JSON.parse(savedTodos) : [];
+    } catch (err) {
+      return [];
+    }
   });
   const [newTodo, setNewTodo] = useState("");
   const [dueDate, setDueDate] = useState("");
-  const [filter, setFilter] = useState("all") ;
+  const [filter, setFilter] = useState("all");
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
@@ -33,18 +37,22 @@ function App() {
     setTodos(newTodos);
   };
 
-  const filteredTodos = todos.filter(todo => {
+  const filteredTodos = todos.filter((todo) => {
     if (filter === "completed") return todo.completed;
     if (filter === "pending") return !todo.completed;
     return true;
   });
 
-  const pendingTodos = todos.filter(todo => !todo.completed).length;
+  const pendingTodos = todos.filter((todo) => !todo.completed).length;
 
   return (
     <div className="container">
-      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-slate-800 md:text-5xl lg:text-6xl">Mi lista ToDo</h1>
-      <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-600">Tareas pendientes: {pendingTodos}</p>
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-slate-800 md:text-5xl lg:text-6xl">
+        Mi lista ToDo
+      </h1>
+      <p className="mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-600">
+        Tareas pendientes: {pendingTodos}
+      </p>
       <input
         type="text"
         value={newTodo}
@@ -56,7 +64,10 @@ function App() {
         value={dueDate}
         onChange={(e) => setDueDate(e.target.value)}
       />
-      <button className='addButton' onClick={handleAddTodo}> <p className="font-bold">Agregar</p> </button>
+      <button className="addButton" onClick={handleAddTodo}>
+        {" "}
+        <p className="font-bold">Agregar</p>{" "}
+      </button>
       <div className="filter-buttons">
         <button onClick={() => setFilter("all")}>Todas</button>
         <button onClick={() => setFilter("completed")}>Completadas</button>
